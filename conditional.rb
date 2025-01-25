e7ce01b6df208cc8b50a8e8b18c151dde5770b0f
rian <rianpr20@gmail.com>
2025-01-26 02:38:45 +0700

    find max number

--- conditional.rb
+++ conditional.rb
@@ -7,3 +7,11 @@ if name[0] == "T" || name[0] == "t"
 else 
   puts "Hello, #{name}"
 end
+
+numbers = [2, 9, 3, 5, 2]
+max = numbers[0]
+
+numbers.each do |n|
+    max = n if n >= max
+end
+print "The max of following elements #{numbers} is #{max}"
