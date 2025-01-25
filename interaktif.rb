0d8be542e5d50a4ac72b4e95905b4b4425c740f9
rian <rianpr20@gmail.com>
2025-01-25 19:28:17 +0700

    input and output

---
+++ interaktif.rb
@@ -0,0 +1,3 @@
+print "what's your name ? "
+name = gets
+puts "Hello #{name}"
--- variabel.rb
+++ variabel.rb
@@ -1,4 +1,20 @@
 mahasiswa = "Tiara"
-nilai = 90
+nilai = "90"
 
-puts "Hai #{mahasiswa} nilai kamu #{nilai}"
+convert_nilai = nilai.to_i
+
+puts convert_nilai
+
+sentance = "hai #{mahasiswa} nilai kamu #{nilai}".capitalize
+
+puts sentance
+
+# split the sentance into an array of words
+words = sentance.split(" ")
+
+# capitalize the first letter of each words
+capitalize_words = words.map {|words|words.capitalize}
+
+capitalize_sentance = capitalize_words.join(" ")
+
+puts capitalize_sentance
