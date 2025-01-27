8b20a304cee280f979aa8f7b00cf6e50eb7e9e52
rian <rianpr20@gmail.com>
2025-01-27 23:10:07 +0700

    looping

---
+++ looping.rb
@@ -0,0 +1,20 @@
+x = 0
+
+#for
+for i in 1..5
+  puts "#{i} zombies incoming!"
+end
+
+#while
+while x < 10 do 
+    puts "z is #{x}"
+    x += 1
+end
+
+#loop 
+loop do
+    puts "x is #{x}"
+    x += 1
+   break if x == 20
+end
+
