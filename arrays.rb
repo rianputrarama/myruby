2e353cf1c8603af5eafcef4cc7cdf4d8d2b30e59
rian <rianpr20@gmail.com>
2025-01-29 07:24:25 +0700

    arrays

---
+++ arrays.rb
@@ -0,0 +1,9 @@
+listOrdered = ["milk","tissue","candy"]
+
+listOrdered.push("pasta")
+
+listOrderedWord = listOrdered.map {|listOrdered|listOrdered.capitalize}
+
+listOrderedMapping = listOrderedWord.join(", ")
+
+puts listOrderedMapping
